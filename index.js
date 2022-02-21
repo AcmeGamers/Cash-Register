bc85d0502c4349bb98ac258820e510398ee35e13
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-02-21 14:10:24 +0500

    well then

--- index.js
+++ index.js
@@ -11,9 +11,59 @@ function checkCashRegister(price, cash, cid) {
     "ONE HUNDRED": 100.0,
   };
 
+  let change = cash - price;
+  let changeArray = [];
   let totalCID = 0;
   for (let element of cid) {
     totalCID += element[1];
   }
-  
+  totalCID = totalCID.toFixed(2);
+  if (change > totalCID) {
+    return { status: "INSUFFICIENT_FUNDS", change: changeArray };
+  } else if (change.toFixed(2) === totalCID) {
+    return { status: "CLOSED", change: cid };
+  }
+  cid = cid.reverse();
+  for (let elem of cid) {
+    let temp = [elem[0], 0];
+    while (change >= UNIT_AMOUNT[elem[0]] && elem[1] > 0) {
+      temp[1] += UNIT_AMOUNT[elem[0]];
+      elem[1] -= UNIT_AMOUNT[elem[0]];
+      change -= UNIT_AMOUNT[elem[0]];
+      change = change.toFixed(2);
+    }
+    if (temp[1] > 0) {
+      changeArray.push(temp);
+    }
+  }
+  if (change > 0) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  }
+  return { status: "OPEN", change: changeArray };
 }
+
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
+
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
